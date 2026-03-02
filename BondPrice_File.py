aa614d0c01f62a0890b36137fcbfcbf7a9f9ffb8
mx2327-hue <mx2327@columbia.edu>
2026-03-01 21:34:16 -0500

    BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,11 +1,15 @@
-import numpy as py
+import numpy as np
+
 def getBondPrice(y,face,couponRate,m,ppy=1):
-n = m * ppy
-y_period = y / ppy
-coupon = face * couponRate / ppy
-t = np.arange(1, n + 1)
-discount = 1 / (1 + y_period) ** t
-pv_coupons = coupon * discount
-pv_face = face / (1 + y_period) ** n
-price = np.sum(pv_coupons) + pv_face
-return price
+  n = m * ppy
+  y_period = y / ppy
+  coupon = face * couponRate / ppy
+  
+  t = np.arange(1, n + 1)
+  discount = 1 / (1 + y_period) ** t
+  
+  pv_coupons = coupon * discount
+  pv_face = face / (1 + y_period) ** n
+  
+  price = np.sum(pv_coupons) + pv_face
+  return price
